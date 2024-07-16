75f4519358baa393e626bb2ebae61c3ed59f49be
nikalinov <nikitakalinovskij@gmail.com>
2024-07-16 10:22:58 +0100

    Add Dynamic content to list view

--- src/main/java/com/studentjournal/controllers/StudentController.java
+++ src/main/java/com/studentjournal/controllers/StudentController.java
@@ -1,20 +1,43 @@
 package com.studentjournal.controllers;
 
+import com.studentjournal.entities.Student;
+import com.studentjournal.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class StudentController {
+    private final StudentService service;
+
+    @Autowired
+    public StudentController(final StudentService service) {
+        this.service = service;
+    }
+
     @GetMapping("/students")
     public String index() {
         return "redirect:students/list";
     }
 
     @GetMapping("/students/list")
-    public String getStudents(final Model model) {
-        model.addAttribute("message", "AAAAAAAAAAAA");
+    public String getStudents(
+            final Model model,
+            @RequestParam(value="page", defaultValue="0") final int pageNum,
+            @RequestParam(value="size", defaultValue="10")  final int pageSize
+    ) {
+        final Page<Student> page = service.getStudents(pageNum, pageSize);
+        final int currPageNum = page.getNumber();
+        final int prevPageNum = page.hasPrevious() ? currPageNum - 1 : -1;
+        final int nextPageNum = page.hasNext() ? currPageNum + 1 : -1;
+
+        model.addAttribute("students", page.getContent());
+        model.addAttribute("prevPageNum", prevPageNum);
+        model.addAttribute("nextPageNum", nextPageNum);
         return "students/list";
     }
 
